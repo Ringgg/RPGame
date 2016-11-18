3cef60915e133b2affec922573bc022d3cb15f8a
Luke Nizik <elejdor@gmail.com>
2016-11-18 17:03:07 +0100

    Top down character movement

---
+++ Assets/Scripts/TopDownPlayerController/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform m_target;
+
+    Vector3 m_offset;
+
+	// Use this for initialization
+	void Start () {
+        m_offset =  transform.position - m_target.position;
+	}
+	
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp( transform.position, m_target.position + m_offset, 0.3f );
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
---
+++ Assets/Scripts/TopDownPlayerController/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public class CharacterMovement
+    {
+        Transform m_transform; // movement target transform
+        Rigidbody m_rigidbody;
+        float m_speed;
+        float m_forceMul;
+        float m_desiredAngle;
+
+        public CharacterMovement( Transform trans, Rigidbody rb, float forceMul )
+        {
+            m_transform = trans;
+            m_rigidbody = rb;
+            m_forceMul = forceMul;
+        }
+
+        public void Tick( float dt )
+        {
+            m_rigidbody.AddForce( m_speed * m_forceMul * m_transform.forward * dt );
+
+        }
+
+        public void SetDirection( Vector3 dir )
+        {
+            dir.Normalize();
+            float desiredAngle = Quaternion.FromToRotation( Vector3.forward, dir ).eulerAngles.y;
+            float currentAngle = Quaternion.FromToRotation( Vector3.forward, m_transform.forward ).eulerAngles.y;
+
+
+            if ( desiredAngle - currentAngle >= 180.0f )
+            {
+                desiredAngle -= 360.0f;
+            }
+            else if ( desiredAngle - currentAngle <= -180.0f )
+            {
+                desiredAngle += 360.0f;
+            }
+
+            float angle = Mathf.Lerp( currentAngle, desiredAngle, 0.4f );
+
+            m_rigidbody.MoveRotation( Quaternion.AngleAxis( angle, Vector3.up ) );
+        }
+
+        public void SetSpeed( float val )
+        {
+            m_speed = val;
+        }
+    }
+
+    CharacterMovement m_movement;
+    Rigidbody m_rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+
+        m_movement = new CharacterMovement( transform, m_rb, 3000 );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw( "Vertical" );
+        float horizontal = Input.GetAxisRaw( "Horizontal" );
+
+        Vector3 dir = new Vector3( horizontal, 0, vertical );
+        dir.Normalize();
+
+        if ( dir.sqrMagnitude > 0 )
+            m_movement.SetDirection( dir );
+
+        float speed = Mathf.Abs( vertical ) + Mathf.Abs( horizontal );
+        speed = Mathf.Clamp01( speed );
+
+        m_movement.SetSpeed( speed );
+        m_movement.Tick( Time.deltaTime );
+    }
+}
